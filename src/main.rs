e8654a0afc0fd251b59dfc39a920e5cc3ac75a43
Logan Darby <logandarby4@gmail.com>
2025-08-24 23:46:21 -0400

    Add more instructions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use crossterm::{self, execute};
+use crossterm::{self, cursor::Show, execute, terminal::LeaveAlternateScreen};
 use std::{
     fs,
     io::{self, Write},
@@ -73,33 +73,121 @@ impl fmt::Display for RawInstruction {
 }
 
 #[derive(Debug)]
-enum Instruction {
-    ClearScreen,
-    Jump(Address),
-    SetReg(Register, Immediate8),
-    AddReg(Register, Immediate8),
-    SetIndex(Address),
-    Draw(Register, Register, Immediate4),
+enum RegOperation {
+    Set,
+    OR,
+    AND,
+    XOR,
+    Add,
+    Sub,
+    SubInv,
+    ShiftLeft,
+    ShiftRight,
 }
-/*
-    Implementing the following first:
-    00E0 (clear screen)
-    1NNN (jump)
-    6XNN (set register VX)
-    7XNN (add value to register VX)
-    ANNN (set index register I)
-    DXYN (display/draw)
-*/
+
+#[derive(Debug, PartialEq)]
+enum SkipIf {
+    Eq,
+    NotEq,
+}
+
+#[derive(Debug)]
+enum Instruction {
+    // Draw
+    ClearScreen,
+    Draw(Register, Register, Immediate4),
+    SetFont(Register),
+    // Subroutine
+    Jump(Address),
+    JumpWithOffset(Address),
+    CallSubroutine(Address),
+    Return,
+    // Controlflow
+    Skip(SkipIf, Register, Immediate8),
+    SkipReg(SkipIf, Register, Register),
+    // Keys
+    SkipKeyPress(SkipIf, Register),
+    GetKey(Register),
+    // Register Logic
+    RegOp(RegOperation, Register, Register),
+    SetRegImmediate(Register, Immediate8),
+    AddRegImmediate(Register, Immediate8),
+    Random(Register, Immediate8),
+    // Store & Load
+    StoreAddr(Register),
+    LoadAddr(Register),
+    //Timers
+    SetSoundTimer(Register),
+    SetDelayTimer(Register),
+    GetDelayTimer(Register),
+    // Index
+    SetIndex(Address),
+    AddIndex(Register),
+    // Misc
+    BinaryDecimalConv(Register),
+}
+
 impl Instruction {
     fn decode(raw: &RawInstruction) -> Option<Instruction> {
         let (nibble1, nibble2, nibble3, nibble4) = raw.to_nibbles();
         Some(match (nibble1, nibble2, nibble3, nibble4) {
+            // Display/Draw
             (0, 0, 0xE, 0) => Instruction::ClearScreen,
-            (0x01, _, _, _) => Instruction::Jump(raw.nnn()),
-            (0x06, _, _, _) => Instruction::SetReg(raw.x(), raw.nn()),
-            (0x07, _, _, _) => Instruction::AddReg(raw.x(), raw.nn()),
-            (0x0A, _, _, _) => Instruction::SetIndex(raw.nnn()),
-            (0x0D, _, _, _) => Instruction::Draw(raw.x(), raw.y(), raw.n()),
+            (0xD, _, _, _) => Instruction::Draw(raw.x(), raw.y(), raw.n()),
+            (0xF, _, 0x2, 0x9) => Instruction::SetFont(raw.x()),
+
+            // Subroutine
+            (0x1, _, _, _) => Instruction::Jump(raw.nnn()),
+            (0xB, _, _, _) => Instruction::JumpWithOffset(raw.nnn()),
+            (0x0, 0x0, 0xE, 0xE) => Instruction::Return,
+            (0x2, _, _, _) => Instruction::CallSubroutine(raw.nnn()),
+
+            // Control Flow
+            (0x3, _, _, _) => Instruction::Skip(SkipIf::Eq, raw.x(), raw.nn()),
+            (0x4, _, _, _) => Instruction::Skip(SkipIf::NotEq, raw.x(), raw.nn()),
+            (0x5, _, _, 0x0) => Instruction::SkipReg(SkipIf::Eq, raw.x(), raw.y()),
+            (0x9, _, _, 0x0) => Instruction::SkipReg(SkipIf::NotEq, raw.x(), raw.y()),
+
+            // Keys
+            (0xF, _, 0x0, 0xA) => Instruction::GetKey(raw.x()),
+            (0xE, _, 0x9, 0xE) => Instruction::SkipKeyPress(SkipIf::Eq, raw.x()),
+            (0xE, _, 0xA, 0x1) => Instruction::SkipKeyPress(SkipIf::NotEq, raw.x()),
+
+            // Register Logic
+            (0x6, _, _, _) => Instruction::SetRegImmediate(raw.x(), raw.nn()),
+            (0x7, _, _, _) => Instruction::AddRegImmediate(raw.x(), raw.nn()),
+            (0x8, _, _, op) => {
+                let reg_op: RegOperation = match op {
+                    0x0 => RegOperation::Set,
+                    0x1 => RegOperation::OR,
+                    0x2 => RegOperation::AND,
+                    0x3 => RegOperation::XOR,
+                    0x4 => RegOperation::Add,
+                    0x5 => RegOperation::Sub,
+                    0x7 => RegOperation::SubInv,
+                    0x6 => RegOperation::ShiftRight,
+                    0xE => RegOperation::ShiftLeft,
+                    _ => return None,
+                };
+                Instruction::RegOp(reg_op, raw.x(), raw.y())
+            }
+
+            // Store and Load
+            (0xF, _, 0x5, 0x5) => Instruction::StoreAddr(raw.x()),
+            (0xF, _, 0x6, 0x5) => Instruction::LoadAddr(raw.x()),
+
+            // Timers
+            (0xF, _, 0x0, 0x7) => Instruction::GetDelayTimer(raw.x()),
+            (0xF, _, 0x1, 0x5) => Instruction::SetDelayTimer(raw.x()),
+            (0xF, _, 0x1, 0x8) => Instruction::SetSoundTimer(raw.x()),
+
+            // Index
+            (0xA, _, _, _) => Instruction::SetIndex(raw.nnn()),
+            (0xF, _, 0x1, 0xE) => Instruction::AddIndex(raw.x()),
+
+            // Misc
+            (0xC, _, _, _) => Instruction::Random(raw.x(), raw.nn()),
+            (0xF, _, 0x3, 0x3) => Instruction::BinaryDecimalConv(raw.x()),
             _ => return None,
         })
     }
@@ -193,15 +281,27 @@ impl Screen {
     }
 }
 
+impl Drop for Screen {
+    fn drop(&mut self) {
+        crossterm::queue!(
+            std::io::stdout(),
+            crossterm::terminal::Clear(crossterm::terminal::ClearType::All)
+        )
+        .unwrap();
+        std::io::stdout().flush().unwrap();
+        let _ = execute!(std::io::stdout(), LeaveAlternateScreen, Show);
+    }
+}
+
 struct Chip8 {
     screen: Screen,
     memory: [u8; Self::MEMORY_SIZE],
-    pc_r: u16,    // Program Counter
-    index_r: u16, // Index Register
+    pc_r: u16,                         // Program Counter
+    index_r: u16,                      // Index Register
     gen_r: [u8; Self::REGISTER_COUNT], // General Purpose Registers
-                  // stack
-                  // delay timer
-                  // sound timer
+    stack: Vec<u16>,                   // Stack
+                                       // delay timer
+                                       // sound timer
 }
 
 impl Chip8 {
@@ -229,6 +329,7 @@ impl Chip8 {
         0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
         0xF0, 0x80, 0xF0, 0x80, 0x80, // F
     ];
+    const BYTES_PER_FONT: u16 = 5;
 
     fn new() -> Self {
         Self {
@@ -237,6 +338,7 @@ impl Chip8 {
             gen_r: [0; Self::REGISTER_COUNT],
             pc_r: Self::ENTRY_POINT,
             index_r: 0,
+            stack: Vec::new(),
         }
     }
 
@@ -257,8 +359,16 @@ impl Chip8 {
         return &mut self.gen_r[Self::REGISTER_COUNT - 1];
     }
 
-    fn register_val(&mut self, reg: &Register) -> &mut u8 {
+    fn register_val_ref(&mut self, reg: &Register) -> &mut u8 {
         return &mut self.gen_r[reg.0 as usize];
+    }
+
+    fn register_val(&self, reg: &Register) -> u8 {
+        return self.gen_r[reg.0 as usize];
+    }
+
+    fn register_set(&mut self, reg: &Register, value: u8) {
+        self.gen_r[reg.0 as usize] = value;
     }
 
     fn load_from_addr(&self, addr: u16) -> u8 {
@@ -286,6 +396,92 @@ impl Chip8 {
         );
     }
 
+    fn execute_font_char(&mut self, reg: &Register) {
+        self.index_r = Self::FONT_START_ADDR + (reg.0 as u16 * Self::BYTES_PER_FONT); 
+    }
+
+    fn execute_reg_op(&mut self, reg_op: &RegOperation, regx: &Register, regy: &Register) {
+        let vx = self.register_val(regx);
+        let vy = self.register_val(regy);
+        // TODO: Carry flag
+        match *reg_op {
+            RegOperation::Set => {
+                self.register_set(regx, vy);
+            }
+            RegOperation::OR => {
+                self.register_set(regx, vx | vy);
+            }
+            RegOperation::XOR => {
+                self.register_set(regx, vx ^ vy);
+            }
+            RegOperation::AND => {
+                self.register_set(regx, vx & vy);
+            }
+            RegOperation::Add => {
+                let (result, overflow) = vx.overflowing_add(vy);
+                self.register_set(regx, result);
+                *self.vf() = overflow as u8;
+            }
+            RegOperation::Sub => {
+                let (result, _) = vx.overflowing_sub(vy);
+                self.register_set(regx, result);
+                *self.vf() = if vx > vy { 1 } else { 0 };
+            }
+            RegOperation::SubInv => {
+                let (result, _) = vy.overflowing_sub(vx);
+                self.register_set(regx, result);
+                *self.vf() = if vy > vx { 1 } else { 0 };
+            }
+            RegOperation::ShiftLeft => {
+                todo!()
+            }
+            RegOperation::ShiftRight => {
+                todo!()
+            }
+        }
+    }
+
+    // Draws sprite N pixels tall located at the index register
+    // at the coordinate x, y in the regX and regY registers respectively
+    // All the pixels that are "on" in the sprite will flip the screen.
+    // If a pixel is turned off this way, the VF register is set to 1. Otherwise, it's set
+    // to 0
+    // The starting coordinate wraps, but the drawing is clipped
+    fn execute_draw(&mut self, regx: &Register, regy: &Register, row_count: &Immediate4) {
+        let start_x = self.register_val(regx) % Screen::N_COLS;
+        let start_y = self.register_val(regy) % Screen::N_ROWS;
+        *self.vf() = 0;
+        let index_addr = self.index_r;
+
+        for row in 0..row_count.0 {
+            let y = start_y + row;
+            if y >= Screen::N_ROWS {
+                break;
+            }
+
+            let sprite_data = self.load_from_addr(index_addr + row as u16);
+
+            for bit_pos in 0..8 {
+                let x = start_x + bit_pos;
+                if x >= Screen::N_COLS {
+                    break;
+                }
+
+                let sprite_bit = (sprite_data >> (7 - bit_pos)) & 1;
+                if sprite_bit == 1 {
+                    let pixel = self.screen.get_pixel(x, y).unwrap();
+                    if pixel {
+                        self.screen.set_pixel(x, y, false);
+                        *self.vf() = 1;
+                    } else {
+                        self.screen.set_pixel(x, y, true);
+                    }
+                }
+            }
+        }
+        self.screen.flush();
+    }
+
     fn execute(&mut self, inst: &Instruction) {
         use Instruction::*;
         match inst {
@@ -294,49 +490,50 @@ impl Chip8 {
                 self.pc_r = addr.0;
                 return;
             }
-            SetReg(reg, value) => *self.register_val(reg) = value.0,
-            AddReg(reg, value) => *self.register_val(reg) += value.0,
+            RegOp(reg_op, regx, regy) => self.execute_reg_op(reg_op, regx, regy),
+            SetRegImmediate(reg, value) => *self.register_val_ref(reg) = value.0,
+            AddRegImmediate(reg, value) => *self.register_val_ref(reg) += value.0,
             SetIndex(addr) => self.index_r = addr.0,
-            // Draws sprite N pixels tall located at the index register
-            // at the coordinate x, y in the regX and regY registers respectively
-            // All the pixels that are "on" in the sprite will flip the screen.
-            // If a pixel is turned off this way, the VF register is set to 1. Otherwise, it's set
-            // to 0
-            // The starting coordinate wraps, but the drawing is clipped
+            AddIndex(_reg) => todo!(),
             Draw(regx, regy, row_count) => {
-                let start_x = *self.register_val(regx) % Screen::N_COLS;
-                let start_y = *self.register_val(regy) % Screen::N_ROWS;
-                *self.vf() = 0;
-                let index_addr = self.index_r;
-
-                for row in 0..row_count.0 {
-                    let y = start_y + row;
-                    if y >= Screen::N_ROWS {
-                        break;
-                    }
-
-                    let sprite_data = self.load_from_addr(index_addr + row as u16);
-
-                    for bit_pos in 0..8 {
-                        let x = start_x + bit_pos;
-                        if x >= Screen::N_COLS {
-                            break;
-                        }
-
-                        let sprite_bit = (sprite_data >> (7 - bit_pos)) & 1;
-                        if sprite_bit == 1 {
-                            let pixel = self.screen.get_pixel(x, y).unwrap();
-                            if pixel {
-                                self.screen.set_pixel(x, y, false);
-                                *self.vf() = 1;
-                            } else {
-                                self.screen.set_pixel(x, y, true);
-                            }
-                        }
-                    }
+                self.execute_draw(regx, regy, row_count);
+            },
+            LoadAddr(_reg) => { todo!() }
+            StoreAddr(_reg) => { todo!() }
+            SetFont(reg) => self.execute_font_char(reg),
+            JumpWithOffset(_addr) => todo!(),
+            CallSubroutine(addr) => {
+                self.stack.push(self.pc_r);
+                self.pc_r = addr.0;
+                return;
+            },
+            Return => {
+                let return_addr = self.stack.pop().expect("CRITICAL: Stack is empty");
+                self.pc_r = return_addr;
+                return;
+            },
+            Skip(skipif, reg, value) => {
+                let eq = self.register_val(reg) == value.0.into();
+                if *skipif == SkipIf::Eq && eq || *skipif == SkipIf::NotEq && !eq {
+                    self.increment_pc();
                 }
-                self.screen.flush();
-            } // _ => panic!("Invalid Instruction {:#?}", inst),
+            },
+            SkipReg(skipif, regx, regy) => {
+                let eq = self.register_val(regx) == self.register_val(regy);
+                if *skipif == SkipIf::Eq && eq || *skipif == SkipIf::NotEq && !eq {
+                    self.increment_pc();
+                }
+            },
+            SkipKeyPress(_skipif, _reg) => todo!(),
+            GetKey(_reg) => todo!(),
+            Random(reg, value) => {
+                let random: u8 = rand::random();
+                self.register_set(reg, value.0 & random);
+            },
+            SetSoundTimer(_reg) => todo!(),
+            SetDelayTimer(_reg) => todo!(),
+            GetDelayTimer(_reg) => todo!(),
+            BinaryDecimalConv(_reg) => todo!(),
         };
         self.increment_pc();
     }
